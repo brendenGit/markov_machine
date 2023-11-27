44daba7ea25e5df34e1afdb919adb495e85ad224
brenden <brendenjarias@gmail.com>
2023-11-26 23:30:22 -0800

    completed

--- makeText.js
+++ makeText.js
@@ -11,35 +11,43 @@ const path = argv[2];
 
 
 async function createWebMM(path, filename) {
-    let content = await axios.get(path);
-    content = content.data;
-    content = stripHtml(content, {
-        onlyStripTags: ["h1", "p"],
-        stripTogetherWithTheirContents: ["h1", "p"],
-    })
-        .filteredTagLocations.reduce(
-            (acc, [from, to]) => `${acc}${content.slice(from, to)}`,
-            ""
-        )
-        .trim();
-
-    content = stripHtml(content).result;
-    return new MarkovMachine(content);
-}
-
-
-function createLocalMM(path) {
-
-    //read and store text data
-    const content = fs.readFileSync(path, 'utf8');
-    return new MarkovMachine(content);
+    try {
+        let content = await axios.get(path);
+        content = content.data;
+        content = stripHtml(content, {
+            onlyStripTags: ["h1", "p"],
+            stripTogetherWithTheirContents: ["h1", "p"],
+        })
+            .filteredTagLocations.reduce(
+                (acc, [from, to]) => `${acc}${content.slice(from, to)}`,
+                ""
+            )
+            .trim();
+    
+        content = stripHtml(content).result;
+        return new MarkovMachine(content);
+    } catch (error) {
+        console.error(`Error reading URL: ${error.message}`);
+        process.exit(1);
+    };
 };
+
+
+async function createLocalMM(path) {
+    try {
+        const content = await fs.readFile(path, 'utf8');
+        return new MarkovMachine(content);
+    } catch (error) {
+        console.error(`Error reading file/URL: ${error.message}`);
+        process.exit(1);
+    }
+}
 
 let markov;
 if (isURL(path)) {
     markov = await createWebMM(path);
 } else {
-    markov = createLocalMM(path);
+    markov = await createLocalMM(path);
 };
 isURL(path)
 
